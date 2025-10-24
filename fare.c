c174b9276185695567ad46c08b880dd86db629e5
agie25-cloud <mukiiagnes0@gmail.com>
2025-10-24 09:04:46 +0300

    Add files via upload

---
+++ fare.c
@@ -0,0 +1,20 @@
+/*
+Name:Agnes Mukii
+Reg no:CT101/G/29254/25
+*/
+# include <stdio.h>
+
+int main (){
+ float distance,fare;
+ 
+ // prompt the user to enter the distance 
+ printf("enter distance traveled (in kilometers)");
+ scanf("%f",&distance);
+ 
+ 
+ fare= distance*50;
+ 
+ //display the result
+ printf("totalfare=%.2f kes\n,fare",fare);
+ return 0;
+ }
