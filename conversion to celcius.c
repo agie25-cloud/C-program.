464fb8b5d495ae13e2cdcf76b42e9c739aff5ac0
agie25-cloud <mukiiagnes0@gmail.com>
2025-10-24 09:03:55 +0300

    Add files via upload

---
+++ conversion to celcius.c
@@ -0,0 +1,26 @@
+/*
+Name:Agnes Mukii
+Reg no:CT101/G/29254/25
+date:23rd October 2025
+*/
+# include <stdio.h>
+// function to convert farnheit to celsius
+float converttocelsius(float farnheit){
+ float celsius;
+ celsius=(farnheit-32)*5/9;
+ return celsius;
+ }
+ int main(){
+ float farnheit,celsius;
+ 
+ // input from user
+ printf("enter temperature in farnheit:");
+ scanf("%f",&farnheit);
+ 
+ // call the function
+ celsius=converttocelsius(farnheit);
+ 
+ // display result
+ printf("temperature in celsius:%.2f\n",celsius);
+ return 0;
+}
